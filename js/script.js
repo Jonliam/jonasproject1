945a05a2393e770dbef0faa4879fd4606213e88c
Jonas <jonaslavado0712@gmail.com>
2023-01-04 18:27:26 +0800

    First Commit

---
+++ js/script.js
@@ -0,0 +1,7 @@
+/* ================================== Typing Animation ===========================*/
+var typed = new Typed(".typing",{
+    strings:["","Web Designer","Ui/Ux Designer","Front-end Web Developer"],
+    typeSpeed:110,
+    BackSpeed:60,
+    loop:true,
+})
